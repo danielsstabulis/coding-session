2e0878be591d09587baa3770d3c330efcae28c1c
Daniels Stabulis <daniels.stabulis@scandiweb.com>
2022-05-06 12:44:20 +0300

    Update App file

--- src/App.js
+++ src/App.js
@@ -1,23 +1,68 @@
-import logo from './logo.svg';
+import { useEffect, useState, useCallback } from 'react';
 import './App.css';
+import ErrorHandler from './components/ErrorHandler/ErrorHandler';
+import { Footer } from './components/Footer/Footer';
+import Input from './components/Input/Input';
+import { FOO, BAR } from './constants/DisplayValues';
+import { INVALID_VALUE } from './constants/Errors';
 
 function App() {
+  const [inputValue, setInputValue] = useState('');
+  const [value, setValue] = useState('');
+  const [ errorHandler, setErrorHandler ] = useState(false)
+
+  const renderHeaderText = () => {
+    return (
+      <div className="App-HeaderText">
+        <h2>Hello There!</h2>
+        <p>This task is demonstrating my knowledge developing web applications.</p>
+      </div>
+    );
+  };
+
+  const onInputChange = (e) => {
+      const { value } = e.target;
+
+      setInputValue(value);
+  };
+
+  const checkForErrors = (value) => {
+    // Add regular expresion that checks if string is number
+    if (value.length > 0 && !(/^\d+$/.test(value))) {
+      setErrorHandler(true);
+      return;
+    }
+
+    setErrorHandler(false);
+  };
+
+  const renderValue = useCallback(() => {
+    const numberInputValue = parseInt(inputValue, 10);
+
+    if (numberInputValue % 15 === 0) {
+      setValue(FOO+BAR)
+    } else if (numberInputValue % 5 === 0) {
+      setValue(FOO)
+    } else if(numberInputValue % 3 === 0) {
+      setValue(BAR)
+    } else {
+      setValue('');
+    }
+  },[inputValue]);
+
+  useEffect(() => {
+      checkForErrors(inputValue);
+      renderValue();
+  },[inputValue, renderValue]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      { renderHeaderText() }
+      <Input labelValue={ 'Input:' } onChange={ onInputChange } value={ inputValue }/>
+      <ErrorHandler type={ errorHandler ? INVALID_VALUE : null}/>
+      <br />
+      <Input labelValue={ 'Output:' } disabled={ true } value={ value } />
+      <Footer />
     </div>
   );
 }
